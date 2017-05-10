63ecdc4f32f1b6be7342db1535b752a39d423678
Markiyan Pyts <markiyan.pyts@osf-global.com>
2017-05-10 22:52:55 +0300

    Worked on server app for tab reloader

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 var WebSocketServer = require("websocketserver");
-var server = new WebSocketServer("all", 9000);
-var config = require('config');
 
-server.on('connection', function(id) {
-    console.log('connection open');
-    server.sendMessage("one", "Welcome to the server!", id);
-});
+class tabReloader {
+    constructor(options) {
+        this.options = options;
+        this.server = false;
+        this.pluginName = "Tab Reloader";
+        this.isConnected = false;
 
-server.on("closedconnection", function(id) {
-    console.log("Connection " + id + " has left the server");
-});
+        this.init();
+    }
 
-server.on("message", function(data, id) {
-    var mes = server.unmaskMessage(data);
-    var str = server.convertToString(mes.message);
-    console.log(str);
-});
+    init() {
+        this.server = new WebSocketServer("all", 9000);
+        this.initListeners();
+    }
 
+    refreshTab(uploadedFiles) {
+        if (this.isConnected) {
+            this.server.sendMessage("all", "Welcome to the server!");
+        } else {
+            console.log('Tab can not be reloaded since browser ' + this.pluginName + ' plugin is not connected to server yet.');
+        }
+    }
+
+    initListeners() {
+        this.server.on('connection', (id) => {
+            console.log('connection open');
+            this.isConnected = true;
+            this.initSocketListeners();
+            //this.server.sendMessage("one", "Welcome to the server!", id);
+        });
+    }
+
+    initSocketListeners() {
+        /*this.socket.onmessage =  (ev) => {
+            console.log(ev.data);
+        };*/
+
+        this.server.on('closedconnection', (id) => {
+            this.isConnected = false;
+            console.log(this.pluginName + ' connection is closed');
+        });
+
+        /*this.server.on('message', (data, id) => {
+            var mes = this.server.unmaskMessage(data);
+            var str = this.server.convertToString(mes.message);
+            console.log(str);
+        });*/
+    }
+};
+
+module.exports = tabReloader;
